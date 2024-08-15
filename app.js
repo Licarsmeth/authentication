3335f150dc8874da79b3bdc3ddc211417f3a6e13
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-08-15 21:09:14 +0545

    added login and passport authentication

--- app.js
+++ app.js
@@ -13,11 +13,18 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index", {user: req.user}));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
 
 const insertUserStmt = db.prepare('INSERT INTO users (username, password) VALUES (?, ?)');
-
 app.post('/sign-up', (req, res, next) => {
   try {
     // Run the prepared statement with user input
@@ -27,6 +34,15 @@ app.post('/sign-up', (req, res, next) => {
     return next(err);
   }
 });
+
+app.post(
+    "/log-in",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/sign-up"
+    })
+  );
+  
 
 
 app.listen(5173, () => console.log("app listening on port 5173")); 
